8fdda5eeb86e516e7d6e85aada507dca59d03f0e
Poowis Kumpai <poowis.k@7solutions.co.th>
2025-07-18 14:52:48 +0700

    fix: cors

--- go.mod
+++ go.mod
@@ -24,8 +24,8 @@ require (
 	github.com/valyala/fasthttp v1.58.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/net v0.31.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 )
--- infrastructure/server/server.go
+++ infrastructure/server/server.go
@@ -4,11 +4,13 @@ import (
 	"wowzaman106/internal/handler/gamehdl"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func New(gh gamehdl.Handler) *fiber.App {
 
 	app := fiber.New()
+
 	// Health check route
 	app.Get("/health", func(c fiber.Ctx) error {
 		return c.SendString("OK")
@@ -16,6 +18,10 @@ func New(gh gamehdl.Handler) *fiber.App {
 
 	// Game handler route
 	app.Post("/game", gh.Play)
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
 
 	return app
 }
